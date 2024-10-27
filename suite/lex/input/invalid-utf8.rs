1086b3d6828918591cee09e3a53c8e6d73d64c84
Connor Horman <chorman64@gmail.com>
2024-10-27 17:01:36 -0400

    fix(invalid-utf8): Warn about saving in vsc breaking the malformed UTF-8

--- suite/lex/input/invalid-utf8.rs
+++ suite/lex/input/invalid-utf8.rs
@@ -3,6 +3,8 @@
 // There's not really much to say about this, except that it contains invalid UTF-8
 // For anyone reading this, the file ends with `\xFF`, if you can't see it.
 // Uh, please don't delete the `\xFF` byte, even accidentally
+// In particular, if you are looking at this in VSC, don't save it, and on't run rustfmt on this
 
 // `\xFF` byte immediately after this comment
-ï¿½
+/*interrupt the stream here*/
+//ÿ
